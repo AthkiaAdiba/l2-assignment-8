cc085aa3715fdae449b1f0a3f8b47e26813d7b6e
thkiaAdiba <athkiaadiba@gmail.com>
2025-04-16 22:15:23 +0600

    added project setup and user model

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import cors from "cors";
+// import { StatusCodes } from "http-status-codes";
+
+const app: Application = express();
+
+// middlewares
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+// app.use("/api/v1", router);
+
+// app.use(globalErrorHandler);
+
+// app.use((req: Request, res: Response, next: NextFunction) => {
+//   res.status(StatusCodes.NOT_FOUND).json({
+//     success: false,
+//     message: "API Not Found!",
+//     error: {
+//       path: req.originalUrl,
+//       message: "Your requested url not found!",
+//     },
+//   });
+// });
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Bike Servicing Management System....");
+});
+
+export default app;
